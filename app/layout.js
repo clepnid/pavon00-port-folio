43ee60bbcd5932913f51a7c6c33ac57a5e035681
clepnid <clepnid@gmail.com>
2024-10-18 09:00:16 +0200

    metadata update

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,13 +10,21 @@ export const metadata = {
   openGraph: {
     title: "Portfolio Pavon",
     description: "Ingeniero software, escritorio virtual versión mínima con proyectos realizados",
-    images: ["/img.png"]
+    images: ["https://pavon00-port-folio.vercel.app/img.png"]
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:image" content={metadata.openGraph.images[0]} />
+        {/* Añade otros metadatos si es necesario */}
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
