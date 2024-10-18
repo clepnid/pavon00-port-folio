5da9361a1e2b7bd856417be382865558dfd31bf7
clepnid <clepnid@gmail.com>
2024-10-18 08:44:45 +0200

    metadata update

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,7 @@ export const metadata = {
   openGraph: {
     title: "Portfolio Pavon",
     description: "Ingeniero software, escritorio virtual versión mínima con proyectos realizados",
-    images: [
-      {
-        url: "/img.png", 
-        width: 800,
-        height: 600,
-        alt: "Imagen representativa del portfolio",
-      },
-    ],
+    images: ["/img.png"]
   },
 };
 
