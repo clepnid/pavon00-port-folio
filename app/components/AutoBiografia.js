4ea83dd3fe2bd654be1bf8eaad826b28d338b14e
clepnid <66835340+clepnid@users.noreply.github.com>
2025-08-19 16:39:20 +0200

    Update AutoBiografia.js

--- app/components/AutoBiografia.js
+++ app/components/AutoBiografia.js
@@ -4,14 +4,15 @@ import './styles/autobiografia.css';
 import './styles/fuentes.css';
 import autobiografiaIcon from './../images/autobiografia.png'
 
-const AutoBiografia = ({funcionBoton}) => {
+const AutoBiografia = ({funcionBoton, funcionBoton2}) => {
     return (
         <div className='divAutobiografia'>
             <div className='textoAutobiografia'>
-                <p className='tituloAutobiografia'>Autobiografía</p>
-                <p className='tiempoAutobiografia'>Tiempo de lectura: 5min</p>
-                <p className='descripcionAutobiografia'>Un libro corto, me he dado cuenta de que soy joven (25 años).</p>
-                <Button style={{fontFamily: 'MS Sans Serif'}} className='botonLeer' onClick={funcionBoton}>Leer</Button>
+                <p className='tituloAutobiografia'>Currículum Vitae</p>
+                <p className='tiempoAutobiografia'>Tiempo de lectura: 1min</p>
+                <p className='descripcionAutobiografia'>He pasado incontables días programando en proyectos, adquiriendo experiencia práctica y resolviendo retos diversos.</p>
+                <Button style={{fontFamily: 'MS Sans Serif'}} className='botonLeer' onClick={funcionBoton}>Español</Button>
+                <Button style={{fontFamily: 'MS Sans Serif', marginLeft: "12px"}} className='botonLeer' onClick={funcionBoton2}>Ingles</Button>
             </div>
             <img src={autobiografiaIcon.src} alt={'autobiografia'} className='iconoAutobiografia' />
         </div>
